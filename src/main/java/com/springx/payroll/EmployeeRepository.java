52b1bc618e67fadcd7a8cc74c625e8460e19eee2
devangadobe <devangadobe@gmail.com>
2023-02-04 22:00:47 +0530

    Added to Main Branch

---
+++ src/main/java/com/springx/payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.springx.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/test/java/com/springx/springx/SpringxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springx.springx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
